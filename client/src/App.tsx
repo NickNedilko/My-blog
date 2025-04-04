141923fe5a0967ea488a484a1c71bb4cf77e3b40
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2025-04-04 14:34:10 +0300

    create pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
 
 
 export default function App() {
   return (
-    <div className='text-3xl'>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
