08e4af710e2acd3ba3b2226b2846e91f8ff7985a
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2025-04-09 20:15:04 +0300

    add update controller and route for user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 import { httpError } from "../utils/http-error.js";
 
 export const getCurrentUser = async (req, res)=>{
@@ -14,3 +16,54 @@ export const getCurrentUser = async (req, res)=>{
     })
 
 } 
+
+
+export const updateUser = async (req, res) => {
+    const user = req.user;
+
+    if (!user) {
+        throw httpError(401, "Unauthorized");
+    }
+
+    const { _id } = user;
+    const { email, userName, avatarUrl, password } = req.body;
+
+ 
+    if (password && password.length < 6) {
+        throw httpError(400, "Password must be at least 6 characters");
+    }
+
+    let passwordHash = undefined;
+    if (password) {
+        passwordHash = await bcrypt.hash(password, 10);
+    }
+
+    if (email) {
+        const existingUser = await User.findOne({ email });
+        if (existingUser && existingUser._id.toString() !== _id.toString()) {
+            throw httpError(400, "Email is already in use");
+        }
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        {
+            email: email || user.email,           // Обновляем, если есть новое значение
+            userName: userName || user.userName,  // То же для имени пользователя
+            avatarUrl: avatarUrl || user.avatarUrl,
+            password: passwordHash || user.password // Обновляем пароль, если он есть
+        },
+        { new: true } 
+    );
+
+    if (!updatedUser) {
+        throw httpError(404, "User not found");
+    }
+
+    res.json({
+        _id: updatedUser._id,
+        email: updatedUser.email,
+        userName: updatedUser.userName,
+        avatarUrl: updatedUser.avatarUrl
+    });
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,12 +2,13 @@ import express from "express";
 
 import ctrlWrapper from "../utils/ctrl.wrapper.js";
 import { authenticate } from "../middlewares/authenticate.js";
-import { getCurrentUser } from "../controllers/user.controller.js";
+import { getCurrentUser, updateUser } from "../controllers/user.controller.js";
 
 
 const router = express.Router();
 
 router.get("/get-user", authenticate, ctrlWrapper(getCurrentUser));
+router.patch("/update-user", authenticate, ctrlWrapper(updateUser));
 
 
 export default router;
