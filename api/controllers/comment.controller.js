e85de25e38c4df50f9a0fe06f871dec269c3d23c
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2025-04-24 23:59:24 +0300

    add like, edit, delete comment controller and route add prettier config

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -19,3 +19,54 @@ export const getPostComments = async (req, res) => {
     }
     res.status(200).json(comments);
 }
+
+
+export const likeComment = async (req, res) => {
+    const { commentId } = req.params;
+    const userId = req.user._id;
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw httpError(404, "Comment not found");
+    }
+    const userIndex = comment.likes.indexOf(userId);
+    if (userIndex === -1) {
+        comment.numberOfLikes += 1;
+        comment.likes.push(userId);
+    } else {
+        comment.numberOfLikes -= 1;
+        comment.likes.splice(userIndex, 1);
+    }
+    await comment.save();
+    res.status(200).json(comment);
+}
+    
+
+export const deleteComment = async (req, res) => {
+    const { commentId } = req.params;
+    const userId = req.user._id;
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw httpError(404, "Comment not found");
+    }
+    if (comment.user.toString() !== userId.toString() && !req.user.isAdmin) {
+        throw httpError(403, "You are not authorized to delete this comment");
+    }
+    await Comment.findByIdAndDelete(commentId);
+    res.status(200).json({ message: "Comment deleted successfully" });
+}
+
+export const updateComment = async (req, res) => {
+    const { commentId } = req.params;
+    const { content } = req.body;
+    const userId = req.user._id;
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw httpError(404, "Comment not found");
+    }
+    if (comment.user.toString() !== userId.toString() && !req.user.isAdmin) {
+        throw httpError(403, "You are not authorized to update this comment");
+    }
+    comment.content = content;
+    await comment.save();
+    res.status(200).json(comment);
+    }
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default  {
+  printWidth: 80,         // максимальная длина строки
+  tabWidth: 2,            // ширина таба в пробелах
+  useTabs: false,         // использовать пробелы вместо табов
+  semi: true,             // ставить точку с запятой
+  singleQuote: true,      // использовать одинарные кавычки
+  trailingComma: 'es5',   // запятые в конце (es5, none, all)
+  bracketSpacing: true,   // пробелы внутри { объектов }
+  arrowParens: 'always',  // скобки у стрелочных функций
+};
